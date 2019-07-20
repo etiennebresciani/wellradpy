7a105587b62352c1f1d1bdea59151464aa68b0e6
ebrescia-PC\ebrescia <etienne.bresciani@flinders.edu.au>
2019-07-20 13:34:42 +0900

    Improve documentation and add default values for threshold criteria

--- examples/drawdown_ex.py
+++ examples/drawdown_ex.py
@@ -7,7 +7,7 @@ Created on 2019/06/26
 
 from wellradpy import drawdown as dr
 
-# Parameters (not all needed, depending on the method; units as you wish, but consistent)
+# Parameters (not all needed, depending on the method)
 t = 1
 T = 10
 S = 1e-4
@@ -16,69 +16,69 @@ rw = 0.15
 sc = 0.05
 alpha = 0.01
 alpha_confidence_level = 0.5
-delta = 0.4 # Restrictions apply (see manuscript)
+delta = 0.4
 
 ###############################################################################
 # Radius of influence
 ###############################################################################
 
-# Method based on absolute drawdown criterion
+# Based on absolute drawdown criterion
 rinfl_absdrawd = dr.rinfl_absdrawd(t, T, S, Q, sc)
 
-# Method based on relative drawdown criterion
-rinfl_reldrawd = dr.rinfl_reldrawd(t, T, S, alpha, rw)
+# Based on relative drawdown criterion
+rinfl_reldrawd = dr.rinfl_reldrawd(t, T, S, rw, alpha)
 
-# Method based on relative flow rate criterion
+# Based on relative flow rate criterion
 rinfl_relflow = dr.rinfl_relflow(t, T, S, alpha)
 
-# Method based on relative volume criterion
+# Based on relative volume criterion
 rinfl_relvol = dr.rinfl_relvol(t, T, S, alpha)
 
-# Quasi-steady state model
+# Based on quasi-steady state model
 rinfl_quasisteady = dr.rinfl_quasisteady(t, T, S)
 
-# Jones'formula
+# Based on Jones'formula
 rinfl_jones = dr.rinfl_jones(t, T, S)
 
-# Extension of closed reservoir regime
+# Based on extension of closed reservoir regime
 rinfl_closedres = dr.rinfl_closedres(t, T, S)
 
-# Impulse response peak
+# Based on impulse response peak
 rinfl_impulse = dr.rinfl_impulse(t, T, S)
 
-# Extension of logarithmic regime
+# Based on extension of logarithmic regime
 rinfl_log = dr.rinfl_log(t, T, S)
 
-# Dimensional analysis
+# Based on dimensional analysis
 rinfl_dim = dr.rinfl_dim(t, T, S)
 
 ###############################################################################
 # Radius of investigation
 ###############################################################################
 
-# Method based on absolute drawdown difference criterion (analysis in normal scale)
-rinv_absdrawddiff_normal = dr.rinv_absdrawddiff_normal(t, T, S, Q, sc)
+# Based on absolute drawdown difference criterion (analysis in lin scale)
+rinv_absdrawddiff_lin = dr.rinv_absdrawddiff_lin(t, T, S, Q, sc)
 
-# Method based on absolute drawdown difference criterion (analysis in log scale)
-rinv_absdrawddiff_log = dr.rinv_absdrawddiff_log(t, T, S, Q, sc, rw)
+# Based on absolute drawdown difference criterion (analysis in log scale)
+rinv_absdrawddiff_log = dr.rinv_absdrawddiff_log(t, T, S, Q, rw, sc)
 
-# Method based on absolute drawdown derivative difference criterion (analysis in normal scale)
-rinv_absdrawdderivdiff_normal = dr.rinv_absdrawdderivdiff_normal(t, T, S, Q, sc, delta)
+# Based on absolute drawdown derivative difference criterion (analysis in lin scale)
+rinv_absdrawdderivdiff_lin = dr.rinv_absdrawdderivdiff_lin(t, T, S, Q, delta, sc)
 
-# Method based on absolute drawdown derivative difference criterion (analysis in log scale)
-rinv_absdrawdderivdiff_log = dr.rinv_absdrawdderivdiff_log(t, T, S, Q, sc, delta, rw)
+# Based on absolute drawdown derivative difference criterion (analysis in log scale)
+rinv_absdrawdderivdiff_log = dr.rinv_absdrawdderivdiff_log(t, T, S, Q, rw, delta, sc)
 
-# Method based on relative drawdown derivative difference criterion (analysis in log scale)
+# Based on relative drawdown derivative difference criterion (analysis in log scale)
 rinv_reldrawdderivdiff_log = dr.rinv_reldrawdderivdiff_log(t, T, S, alpha_confidence_level)
 
-# Semi-empirical start of constant-head boundary effect
+# Based on semi-empirical start of constant-head boundary effect
 rinv_consthead = dr.rinv_consthead(t, T, S)
 
-# Intersection of unbounded and closed boundary regimes
+# Based on intersection of unbounded and closed boundary regimes
 rinv_closedres = dr.rinv_closedres(t, T, S)
 
-# Intersection of unbounded and linear barrier regimes
+# Based on intersection of unbounded and linear barrier regimes
 rinv_linearbarr = dr.rinv_linearbarr(t, T, S)
 
-# Impulse response difference peak
+# Based on impulse response difference peak
 rinv_impulse = dr.rinv_impulse(t, T, S)
--- wellradpy/drawdown.py
+++ wellradpy/drawdown.py
@@ -12,73 +12,285 @@ from .tools import E1, E1inv, Xinv, Yinv, Zinv
 # Radius of influence functions
 ###############################################################################
 
-def rinfl_absdrawd(t, T, S, Q, sc):
+def rinfl_absdrawd(t, T, S, Q, sc=0.05):
     """
-    Calculate radius of influence based on absolute drawdown criterion.
+    Calculate radius of influence during pumping based on an absolute drawdown criterion.
+
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    Q: float
+        Pumping rate.
+    sc: float, optional
+        Absolute drawdown threshold.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2 * np.sqrt(E1inv(4*np.pi*T*sc/Q))
     return C * np.sqrt(T*t/S)
 
-def rinfl_reldrawd(t, T, S, alpha, rw):
+def rinfl_reldrawd(t, T, S, rw, alpha=0.01):
     """
-    Calculate radius of influence based on relative drawdown criterion.
+    Calculate radius of influence during pumping based on an relative drawdown criterion.
+
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    rw: float
+        Well radius.
+    alpha: float, optional
+        Drawdown threshold relative to drawdown at the well.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2 * np.sqrt(E1inv(alpha*E1(S*rw**2/(4*T*t))))
     return C * np.sqrt(T*t/S)
 
-def rinfl_relflow(t, T, S, alpha):
+def rinfl_relflow(t, T, S, alpha=0.01):
     """
-    Calculate radius of influence based on relative flow rate criterion.
+    Calculate radius of influence during pumping based on an relative flow rate criterion.
+
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    alpha: float, optional
+        Flow rate threshold relative to pumping rate.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2 * np.sqrt(-np.log(alpha))
     return C * np.sqrt(T*t/S)
 
-def rinfl_relvol(t, T, S, alpha):
+def rinfl_relvol(t, T, S, alpha=0.01):
     """
-    Calculate radius of influence based on relative volume criterion.
+    Calculate radius of influence during pumping based on an relative volume criterion.
+
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    alpha: float, optional
+        Volume threshold relative to volume of cone of depression.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2 * np.sqrt(Xinv(alpha))
     return C * np.sqrt(T*t/S)
 
 def rinfl_quasisteady(t, T, S):
     """
     Calculate radius of influence based on quasi-steady state model.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2
     return C * np.sqrt(T*t/S)
 
 def rinfl_jones(t, T, S):
     """
     Calculate radius of influence based on Jones'formula.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 4
     return C * np.sqrt(T*t/S)
 
 def rinfl_closedres(t, T, S):
     """
     Calculate radius of influence based on extension of closed reservoir regime.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2.83
     return C * np.sqrt(T*t/S)
 
 def rinfl_impulse(t, T, S):
     """
     Calculate radius of influence based on impulse response peak.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2
     return C * np.sqrt(T*t/S)
 
 def rinfl_log(t, T, S):
     """
     Calculate radius of influence based on extension of logarithmic regime.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 1.5
     return C * np.sqrt(T*t/S)
 
 def rinfl_dim(t, T, S):
     """
     Calculate radius of influence based on dimensional analysis.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of influence.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 1
     return C * np.sqrt(T*t/S)
 
@@ -86,67 +298,269 @@ def rinfl_dim(t, T, S):
 # Radius of investigation functions
 ###############################################################################
 
-def rinv_absdrawddiff_normal(t, T, S, Q, sc):
+def rinv_absdrawddiff_lin(t, T, S, Q, sc=0.05):
     """
-    Calculate radius of investigation based on absolute drawdown difference criterion (analysis in normal scale).
+    Calculate radius of investigation based on absolute drawdown difference criterion when data are analyzed in lin scale.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    Q: float
+        Pumping rate.
+    sc: float, optional
+        Absolute drawdown difference threshold.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = np.sqrt(E1inv(4*np.pi*T*sc/Q))
     return C * np.sqrt(T*t/S)
 
-def rinv_absdrawddiff_log(t, T, S, Q, sc, rw):
+def rinv_absdrawddiff_log(t, T, S, Q, rw, sc=0.05):
     """
-    Calculate radius of investigation based on absolute drawdown difference criterion (analysis in log scale).
+    Calculate radius of investigation based on absolute drawdown difference criterion when data are analyzed in log scale.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    Q: float
+        Pumping rate.
+    sc: float, optional
+        Absolute drawdown difference threshold.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     uw = S * rw**2 / (4*T*t)
     C = np.sqrt(Yinv(4*np.pi*T*sc/Q, uw))
     return C * np.sqrt(T*t/S)
 
-def rinv_absdrawdderivdiff_normal(t, T, S, Q, sc, delta):
+def rinv_absdrawdderivdiff_lin(t, T, S, Q, delta, sc=0.05):
     """
-    Calculate radius of investigation based on absolute drawdown derivative difference criterion (analysis in normal scale).
+    Calculate radius of investigation based on absolute drawdown derivative difference criterion when data are analyzed in lin scale.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    Q: float
+        Pumping rate.
+    delta: float
+        Window size in derivative calculation (note: restrictions apply; see manuscript for details).
+    sc: float, optional
+        Absolute drawdown difference threshold.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = np.sqrt(-np.log(4*np.pi*T*np.sqrt(2)*sc/(Q*delta)))
     return C * np.sqrt(T*t/S)
 
-def rinv_absdrawdderivdiff_log(t, T, S, Q, sc, delta, rw):
+def rinv_absdrawdderivdiff_log(t, T, S, Q, rw, delta, sc=0.05):
     """
-    Calculate radius of investigation based on absolute drawdown derivative difference criterion (analysis in log scale).
+    Calculate radius of investigation based on absolute drawdown derivative difference criterion when data are analyzed in log scale.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    Q: float
+        Pumping rate.
+    delta: float
+        Window size in derivative calculation (note: restrictions apply; see manuscript for details).
+    sc: float, optional
+        Absolute drawdown difference threshold.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     uw = S * rw**2 / (4*T*t)
     C = np.sqrt(Zinv(4*np.pi*T*np.sqrt(2)*sc/(Q*delta), uw))
     return C * np.sqrt(T*t/S)
 
-def rinv_reldrawdderivdiff_log(t, T, S, alpha):
+def rinv_reldrawdderivdiff_log(t, T, S, alpha=0.5):
     """
-    Calculate radius of investigation based on relative drawdown derivative difference criterion (analysis in log scale).
+    Calculate radius of investigation based on relative drawdown derivative difference criterion when data are analyzed in log scale.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    alpha: float, optional
+        Confidence level at which the presence of a linear barrier would be detected.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = np.sqrt(-np.log(np.power(2, alpha)-1))
     return C * np.sqrt(T*t/S)
 
 def rinv_consthead(t, T, S):
     """
     Calculate radius of investigation based on semi-empirical start of constant-head boundary effect.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2.64
     return C * np.sqrt(T*t/S)
 
 def rinv_closedres(t, T, S):
     """
     Calculate radius of investigation based on intersection of unbounded and closed boundary regimes.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 2
     return C * np.sqrt(T*t/S)
 
 def rinv_linearbarr(t, T, S):
     """
     Calculate radius of investigation based on intersection of unbounded and linear barrier regimes.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 0.75
     return C * np.sqrt(T*t/S)
 
 def rinv_impulse(t, T, S):
     """
     Calculate radius of investigation based on impulse response difference peak.
+    
+    Parameters
+    ----------
+    t: float
+        Time from beginning of pumping.
+    T: float
+        Transmissivity.
+    S: float
+        Storativity.
+    
+    Returns
+    -------
+    rinfl: float
+        Radius of investigation.
+    
+    Notes
+    -----
+    Units as you wish, but must be consistent for all the parameters.
+    
     """
+    
     C = 1
     return C * np.sqrt(T*t/S)
